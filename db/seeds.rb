d09c8a85d71112845fa4e6a08da38de81305db5f
Zac-Mwangi <zackmwangi998@gmail.com>
2022-09-08 22:23:18 +0300

    seed the category data in the table

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,11 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+# category
+Category.create(category: "clothing")
+Category.create(category: "laptop")
+Category.create(category: "electronics")
+Category.create(category: "sporting")
+
+
 puts "✅ Done seeding!"
