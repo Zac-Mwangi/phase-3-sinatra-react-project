7399b5f20b01235c5ded4fc1a31402faad99103e
Zac-Mwangi <zackmwangi998@gmail.com>
2022-09-08 22:23:56 +0300

    seed the the users and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,14 @@ Category.create(category: "laptop")
 Category.create(category: "electronics")
 Category.create(category: "sporting")
 
+# users
+User.create(username: "stella" , password: "123")
+User.create(username: "maina" , password: "123")
+
+
+# commennts
+Comment.create(name: "stella" , email: "stl@gmail.com" , message: "hello there")
+Comment.create(name: "maina" , email: "maina@gmail.com" , message: "hello an here")
+
 
 puts "✅ Done seeding!"
